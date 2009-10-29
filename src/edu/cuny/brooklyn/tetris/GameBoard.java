9b6e456dc1fdfa52ddac986afeb22f77578e061f
Ramin Rakhamimov <ramin32@gmail.com>
2009-10-29 16:59:04 -0400

    Added keylistener.

--- src/edu/cuny/brooklyn/tetris/GameBoard.java
+++ src/edu/cuny/brooklyn/tetris/GameBoard.java
@@ -7,38 +7,52 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 import javax.swing.Timer;
 import javax.swing.SwingUtilities;
+import java.util.Random;
 
-import edu.cuny.brooklyn.tetris.shape.*;
-
-public class GameBoard extends JPanel implements Runnable, ActionListener {
+public class GameBoard extends JPanel implements Runnable, ActionListener, KeyListener {
     private static final int ANIMATION_RATE = 5;
     private static final int CELL_SIZE = 10;
     private static final int X_CELLS = 50;
     private static final int Y_CELLS = 100;
-
+    private static final int BOARD_WIDTH = X_CELLS * CELL_SIZE;
+    private static final int BOARD_HEIGHT = Y_CELLS * CELL_SIZE;
+    private static final Random random_ = new Random(System.currentTimeMillis());
 
     private final JFrame frame_;
     private final Timer timer_;
+    private int xPosition = BOARD_WIDTH/2;
     private int yPosition = 0;
     private int velocity = +1;
     private final Shape[] shapes_;
+    private Shape currentShape_;
 
     public GameBoard() {
         frame_ = new JFrame("Tetris");
         frame_.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame_.setFocusable(true);
+        frame_.addKeyListener(this);
 
         setPreferredSize(new Dimension(CELL_SIZE*X_CELLS, CELL_SIZE*Y_CELLS));
+        
         timer_ = new Timer(ANIMATION_RATE,this);
 
-        shapes_ = new Shape[]{ new TShape(CELL_SIZE),
-                               new LShape(CELL_SIZE),
-                               new SquareShape(CELL_SIZE),
-                               new LineShape(CELL_SIZE),
-                               new ZigZagShape(CELL_SIZE) };
-
+        shapes_ = new Shape[]{ Shape.createLineShape(CELL_SIZE),
+                               Shape.createTShape(CELL_SIZE),
+                               Shape.createLShape(CELL_SIZE),
+                               Shape.createZigZagShape(CELL_SIZE),
+                               Shape.createSquareShape(CELL_SIZE) };
+        randomizeCurrentShape();
     }
+    
+    private void randomizeCurrentShape()
+    {
+        currentShape_ = shapes_[random_.nextInt(shapes_.length)];
+    }
+
     public void run() {
         frame_.add(this);
         frame_.pack();
@@ -49,7 +63,7 @@ public class GameBoard extends JPanel implements Runnable, ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
-        if (yPosition < 0 || yPosition > getHeight()) {
+        if (yPosition < 0 || yPosition > getHeight()-currentShape_.getHeight()) {
             velocity *= -1; 
         }
         yPosition += velocity;
@@ -58,7 +72,17 @@ public class GameBoard extends JPanel implements Runnable, ActionListener {
     public void paintComponent(Graphics g) {
         g.clearRect(0,0,getWidth(),getHeight());
         g.setColor(Color.RED);
-        for(int i = 0; i < shapes_.length; i++)
-            shapes_[i].draw(g,i*getWidth()/shapes_.length,yPosition);
+        currentShape_.draw(g,xPosition,yPosition);
     }
+
+    public void keyPressed(KeyEvent e)
+    {	
+        if(e.getKeyCode() == KeyEvent.VK_LEFT)
+            xPosition -= CELL_SIZE;
+        else if(e.getKeyCode() == KeyEvent.VK_RIGHT)
+            xPosition += CELL_SIZE;
+
+    }
+    public void keyReleased(KeyEvent e){}
+    public void keyTyped(KeyEvent e){}
 }
