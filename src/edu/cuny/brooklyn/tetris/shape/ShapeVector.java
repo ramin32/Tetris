8e3840b5caed76987de0951a362fc37e904031aa
Ramin Rakhamimov <ramin32@gmail.com>
2009-12-10 13:48:38 -0500

    Added more shapes.

--- src/edu/cuny/brooklyn/tetris/shape/ShapeVector.java
+++ src/edu/cuny/brooklyn/tetris/shape/ShapeVector.java
@@ -38,6 +38,14 @@ public interface ShapeVector {
         new Point(1,1),
         new Point(2,1) 
     };
+    
+
+    Point[] REVERSED_L = {
+        new Point(0,0),
+        new Point(0,1),
+        new Point(0,2),
+        new Point(1,2)
+    };
 
     Point[] ZIG_ZAG = { 
         new Point(0,0),
@@ -46,8 +54,15 @@ public interface ShapeVector {
         new Point(2,1) 
     };
 
+    Point[] REVERSED_ZIG_ZAG = {
+        new Point(0,1),
+        new Point(1,1),
+        new Point(1,0),
+        new Point(2,0)
+    };
+    
     /**
      * An array of all the shape vectors in this class.
      */
-    Point[][] VECTORS = {T, L, LINE, ZIG_ZAG, SQUARE};
+    Point[][] VECTORS = {T, L, REVERSED_L, LINE, ZIG_ZAG, REVERSED_ZIG_ZAG, SQUARE};
 }
